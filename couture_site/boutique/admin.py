54ef9a8aa4c60c231b94a6ea6b429e47874fbd6d
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2025-05-19 22:14:42 +0200

    Editing admin models

--- couture_site/boutique/admin.py
+++ couture_site/boutique/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cotisant, Produit, Achat
 
 # Register your models here.
+
+admin.site.register(Cotisant)
+admin.site.register(Produit)
+admin.site.register(Achat)
