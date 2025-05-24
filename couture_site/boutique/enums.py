ca4014ea6897406781eb8ef6acb61337a33140e8
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2025-05-24 16:43:13 +0200

    Bug fix with Produit model

---
+++ couture_site/boutique/enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class ProductCategory(Enum):
+    CHUTE = "Chute"
+    PELOTE = "Pelote"
+    TISSUE = "Tissu"
+    WATTE = "Watte"
+    OUTILS = "Outils"
+    RANGEMENT = "Rangement"
+    CREATION = "Création"
+    ACCESSOIRES = "Accessoires"
+    AUTRE = "Autre"
--- couture_site/boutique/models.py
+++ couture_site/boutique/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .enums import ProductCategory
 
 # Create your models here.
 
@@ -8,8 +9,7 @@ class Cotisant(models.Model):
     prenom = models.CharField(max_length=100)
     solde = models.DecimalField(max_digits=7, decimal_places=2, default=0.0)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = user.email
-
+    email = User.email
     def __str__(self):
         return f"{self.nom.upper()} {self.prenom.capitalize()} ({self.solde} crédits)"
 
@@ -19,6 +19,11 @@ class Produit(models.Model):
     description = models.TextField(blank=True)
     prix = models.DecimalField(max_digits=7, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
+    vendable = models.BooleanField(default=False)
+    categorie = models.CharField(ProductCategory, max_length=20, 
+                                 choices=[(tag.name, tag.value) for tag in ProductCategory], 
+                                 default=ProductCategory.AUTRE.name)
+
 
     def __str__(self):
         return f"{self.nom} ({self.stock} en stock)"
